5b654f9cc605572a717de0bc1bce1d99af818f22
pawelitex3 <38460805+pawelitex3@users.noreply.github.com>
2021-01-22 03:35:57 +0100

    Update hopfield.py

--- hopfield.py
+++ hopfield.py
@@ -1,21 +1,25 @@
 import numpy as np
-import cv2
 import pygame
 import random
+import cv2
+import matplotlib.pyplot as plt
 
 pygame.init()
-numbers = [str(i+1) for i in range(20)]
-
-print(np.version.version)
+numbers = [str(i+1) for i in range(10)]
 
 class HopfieldNewtork(object):
   def __init__(self, N):
     self.N = N
     self.weights = np.zeros((self.N, self.N))
-    self.iterations = 15000
+    self.iterations = 5000
 
   def little_dynamics(self, state):
-    #Zadanie domowe!
+    state = np.dot(self.weights, np.transpose(state))
+    for i in range(len(state)):
+      if state[i] < 0:
+        state[i] = -1
+      else:
+        state[i] = 1
     return state
 
   def glauber_dynamics(self, state):
@@ -28,11 +32,17 @@ class HopfieldNewtork(object):
     return state
 
   def run_dynamics(self, state, dtype='glauber'):
+    energy = []
     for i in range(self.iterations):
       if dtype == 'glauber':
         state = self.glauber_dynamics(state)
+        e = -0.5*np.dot(np.dot(np.transpose(state), self.weights), state)
+        energy.append(e)
       else:
         state = self.little_dynamics(state)
+    plt.plot(range(len(energy)), energy)
+    plt.savefig('energy.pdf')
+    plt.close()
 
   def training(self, training_data):
     P = len(training_data)
@@ -47,13 +57,15 @@ class HopfieldNewtork(object):
     P = len(training_data)
     hamming_distance = np.zeros((self.iterations, P))
     for iteration in range(self.iterations):
-      for i in range(self.N**2):
+      a = np.zeros(self.N)
+      for i in range(self.N):
         a[i] = 0
-        for j in range(self.N**2):
-          a[i] += self.weights[i,j] * state[p, j]
+        for j in range(self.N):
+          a[i] += self.weights[i,j] * state[j]
       state = np.where(a < 0, -1, 1)
-      for p in range(self.P):
+      for p in range(P):
         hamming_distance[iteration, p] = (state - training_data[p] != 0).sum()
+      print(hamming_distance)
 
 
 class Button(object):
@@ -81,11 +93,8 @@ def create_buttons(buttons):
     row.append(Button(100, 620, 80, 30, 'clear'))
     row.append(Button(190, 620, 80, 30, 'noise'))
     row.append(Button(280, 620, 80, 30, 'negative'))
-    for i in range(10):
+    for i in range(4):
         row.append(Button(10+i*60, 660, 50, 30, str(i+1)))
-
-    for i in range(10):
-        row.append(Button(10+i*60, 700, 50, 30, str(i+11)))
 
     buttons.append(row)
 
@@ -125,7 +134,7 @@ for i in range(2500):
 
 
 images = []
-for i in range(3):
+for i in range(4):
     filename = str(i+1) + ".png"
     img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
     ret, binary_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
@@ -141,6 +150,8 @@ displaying_points = []
 create_points(displaying_points, current_points)
 draw_image(window, displaying_points)
 
+white[white == 255] = -1
+
 create_buttons(buttons)
 draw_buttons(buttons, window)
 pygame.display.flip()
@@ -153,6 +164,7 @@ for image in images:
     image[image == 255] = -1
 
 h = HopfieldNewtork(2500)
+
 h.training(images)
 
 while running:
@@ -161,12 +173,34 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
+          if mouse[1] < 620:
+            for i in range(len(displaying_points)):
+              if mouse[0] >= displaying_points[i].x and mouse[0] < displaying_points[i].x + 10 and mouse[1] >= displaying_points[i].y and mouse[1] < displaying_points[i].y + 10:
+                current_points[i] *= (-1)
+                current_points[current_points == 1] = 0
+                current_points[current_points == -1] = 255
+                displaying_points = []
+                create_points(displaying_points, current_points)
+                draw_image(window, displaying_points)
+                draw_buttons(buttons, window)
+                current_points[current_points == 255] = -1
+                current_points[current_points == 0] = 1
+                pygame.display.flip()
+          else:
                 for row in buttons:
                     for button in row:
                         if button.was_clicked(mouse[0], mouse[1]):
                             if button.text == 'run':
                                 window.fill((0, 0, 0))
                                 h.run_dynamics(current_points)
+                                differences = []
+                                for image in images:
+                                  diff = (image - current_points != 0).sum()
+                                  differences.append(diff)
+                                font = pygame.font.SysFont('verdana', 20)
+                                t = f'number of difference: {min(differences)}'
+                                text = font.render(t, 1, (255, 255, 0))
+                                window.blit(text, (350, 680))
                                 current_points[current_points == 1] = 0
                                 current_points[current_points == -1] = 255
                                 displaying_points = []
@@ -179,9 +213,11 @@ while running:
                             elif button.text == 'clear':
                                 window.fill((0, 0, 0))
                                 current_points = np.copy(white)
+                                current_points[current_points == -1] = 255
                                 displaying_points = []
                                 create_points(displaying_points, current_points)
                                 draw_image(window, displaying_points)
+                                current_points[current_points == 255] = -1
                                 draw_buttons(buttons, window)
                                 pygame.display.flip()
                             elif button.text == 'noise':
